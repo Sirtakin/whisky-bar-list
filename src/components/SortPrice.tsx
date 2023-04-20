9b06b07862cd221f2ae0d4728c2e211d7c59d878
sirtakin <kristian.garberg@gmail.com>
2023-04-21 00:10:43 +0200

    Made inputfield on price filter button

--- src/components/SortPrice.tsx
+++ src/components/SortPrice.tsx
@@ -3,6 +3,14 @@ import {
   RangeSliderTrack,
   RangeSliderFilledTrack,
   RangeSliderThumb,
+  Box,
+  Flex,
+  GridItem,
+  Grid,
+  Text,
+  Spacer,
+  NumberInput,
+  NumberInputField,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { Whisky } from "../hooks/useWhisky";
@@ -21,23 +29,62 @@ export const SortPrice = ({ priceRange }: Props) => {
 
   const [selectedPrice, setSelectedPrice] = useState([minPrice, maxPrice]);
 
+  const handleChange = (selectedPrice: number[]) =>
+    setSelectedPrice(selectedPrice);
+
+  const handleMinInput = (value: number) => {
+    setSelectedPrice(selectedPrice.map((v, i) => (i === 0 ? value : v)));
+  };
+
   console.log(selectedPrice);
 
   return (
     <>
-      <RangeSlider
-        aria-label={["min", "max"]}
-        defaultValue={[minPrice, maxPrice]}
-        min={price[0]}
-        max={price[price.length - 1]}
-        onChangeEnd={(val) => setSelectedPrice(val)}
-      >
-        <RangeSliderTrack>
-          <RangeSliderFilledTrack />
-        </RangeSliderTrack>
-        <RangeSliderThumb index={0} />
-        <RangeSliderThumb index={1} />
-      </RangeSlider>
+      <Flex mb={3}>
+        <Box>
+          <Text>{selectedPrice[0]}</Text>
+          <NumberInput
+            maxW="100px"
+            mr="2rem"
+            inputMode="decimal"
+            min={selectedPrice[0]}
+            defaultValue={selectedPrice[0]}
+            value={selectedPrice[0]}
+            onChange={handleMinInput}
+          >
+            <NumberInputField />
+          </NumberInput>
+        </Box>
+        <Spacer />
+        <Box>
+          <Text>{selectedPrice[1]}</Text>
+        </Box>
+      </Flex>
+      <Box m={2}>
+        <RangeSlider
+          aria-label={["min", "max"]}
+          defaultValue={[minPrice, maxPrice]}
+          min={price[0]}
+          max={price[price.length - 1]}
+          onChangeEnd={handleChange}
+        >
+          <RangeSliderTrack>
+            <RangeSliderFilledTrack />
+          </RangeSliderTrack>
+          <RangeSliderThumb
+            index={0}
+            fontSize="sm"
+            boxSize="32px"
+            children={selectedPrice[0]}
+          />
+          <RangeSliderThumb
+            index={1}
+            fontSize="sm"
+            boxSize="32px"
+            children={selectedPrice[1]}
+          />
+        </RangeSlider>
+      </Box>
     </>
   );
 };
